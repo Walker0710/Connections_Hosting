a2f817bec5fefb50a8b3b7a025db27e71aa66819
AnkushSingh0710 <ankushsingh00710@gmail.com>
2024-08-21 00:24:13 +0530

    console

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
+import {HashRoute} from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HashRoute>
     <App />
+    </HashRoute>
   </StrictMode>,
 )
